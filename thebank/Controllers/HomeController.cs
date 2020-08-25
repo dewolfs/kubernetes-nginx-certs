f1c31b12592b65e7a1657358508642567cbb3312
Samuel DE WOLF <samuel.de.wolf.ext@proximus.com>
2020-08-25 17:13:38 +0200

    Init

---
+++ thebank/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using thebank.Models;
+
+using Prometheus;
+
+namespace thebank.Controllers
+{
+    public class HomeController : Controller
+    {
+        Counter counter = Metrics.CreateCounter("visitors","Number of views on the indexpage");
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            counter.Inc();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
